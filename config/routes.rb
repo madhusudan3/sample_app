3e84f02c8c1b52c47a47f22012dded96b7cbc010
Madhu <madhusudan.koukutla@gmail.com>
2018-09-13 12:51:46 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  
+
   root 'static_pages#home'
   get '/help',      to: 'static_pages#help'
   get '/about',     to: 'static_pages#about'
   get '/contact',   to: 'static_pages#contact'
   get '/news',      to: 'static_pages#news'
   get '/signup',    to: 'users#new'
-  resource :users
+  get '/signup',    to: 'users#create'
+  resources :users
 end
