f919ab342f022e4fa1ce8f3356a70dfe61976b5e
Madhu <madhusudan.koukutla@gmail.com>
2018-09-13 15:45:59 -0500

    basic-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-  get '/help',      to: 'static_pages#help'
-  get '/about',     to: 'static_pages#about'
-  get '/contact',   to: 'static_pages#contact'
-  get '/news',      to: 'static_pages#news'
-  get '/signup',    to: 'users#new'
-  get '/signup',    to: 'users#create'
+  get     '/help',      to: 'static_pages#help'
+  get     '/about',     to: 'static_pages#about'
+  get     '/contact',   to: 'static_pages#contact'
+  get     '/news',      to: 'static_pages#news'
+  get     '/signup',    to: 'users#new'
+  get     '/signup',    to: 'users#create'
+  get     '/login',     to: 'sessions#new'
+  post    '/login',     to: 'sessions#create'
+  delete  '/logout',    to: 'sessions#destroy'
   resources :users
 end
